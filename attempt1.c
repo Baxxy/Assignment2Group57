397d28e0178c24807c8db6163d146dd4bba9d047
Baxxy <k.yanyuk90@gmail.com>
2017-02-24 12:04:54 +0000

    more tests

---
+++ attempt1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Player {
+  char playertype[20];
+  char playername[20];
+  int lifepoints;
+  int smartness;
+  int strength;
+  int magicskills;
+  int luck;
+  int dexterity;
+}; 
+
+int main(void)
+{
+
+
+
+
+
+
+}
